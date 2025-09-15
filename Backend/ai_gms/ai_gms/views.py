7e9dc63885be0396af78c6bd693a65a6c74406ed
abdulwahidjaved <163683209+abdulwahidjaved@users.noreply.github.com>
2025-09-16 02:38:46 +0530

    done

---
+++ Backend/ai_gms/ai_gms/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home/home.html")
+    
+def features(request):
+    return render(request, "features/features.html")
+    
+def about(request):
+    return render(request, "about/about.html")
+    
+def contact(request):
+    return render(request, "contact/contact.html")
+    
---
+++ Backend/ai_gms/static/js/base.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "300px";
+    }
+    else {
+        menuList.style.maxHeight = "0px";
+    }
+}
