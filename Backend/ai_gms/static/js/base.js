fa7bb1daaf59dfece9fb05e6daff788ac8b016db
abdulwahidjaved <163683209+abdulwahidjaved@users.noreply.github.com>
2025-09-16 21:06:18 +0530

    Done

--- Backend/ai_gms/static/js/base.js
+++ Backend/ai_gms/static/js/base.js
@@ -1,9 +1,9 @@
+const menuList = document.getElementById("menu-list");
 const menuIcon = document.getElementById("menu-icon");
-const menuList = document.getElementById("menu-list")
 menuList.style.maxHeight = "0px";
 
 function toggleMenu() {
-    if (menuList.style.maxHeight == "0px") {
+    if( menuList.style.maxHeight == "0px"){
         menuList.style.maxHeight = "300px";
     }
     else {
